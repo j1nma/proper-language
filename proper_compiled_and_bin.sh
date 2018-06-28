3fe996298287109b55935ceb7cfa89568733ae86
j1nma <j1nma@users.noreply.github.com>
2018-06-28 15:31:44 -0300

    mute script commands show compile failed error

--- proper_compiled_and_bin.sh
+++ proper_compiled_and_bin.sh
@@ -1,2 +1,7 @@
 ./compiler/proper_compiler < $1 > proper_compiled.c
-gcc proper_compiled.c run/var/*.c -o proper.out
+@gcc proper_compiled.c run/var/*.c -o proper.out > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+    echo "Compile failed!"
+    exit 1
+fi
